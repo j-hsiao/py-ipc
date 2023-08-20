d53d6816284e9304fd884a10e8b9b9da7694860d
jason hsiao <oaishnosaj@gmail.com>
2023-08-20 17:33:58 +0100

    Implementing generic reader

--- jhsiao/ipc/sockets/threaded/reader.py
+++ jhsiao/ipc/sockets/threaded/reader.py
@@ -1,17 +1,67 @@
-"""Read from sockets."""
+"""Read messages from multiple sockets in a single thread."""
+__all__ = ['Reader']
+import threading
+import sys
 
-class ListenReader(object):
-    """Wrap a listening socket to match reader interface."""
-    def __init__(self, listener, poller, wrappercls):
+if sys.version_info.major > 2:
+    _wait_for_cond = threading.Condition.wait_for
+else:
+    import time
+    def _wait_for_cond(cond, pred, timeout=None):
+        """Wait for pred to be true.
+
+        Assume cond is held before this is called.
+        """
+        if pred():
+            return True
+        if timeout is None:
+            while not pred():
+                cond.wait(None)
+            return True
+        elif timeout == 0:
+            return False
+        else:
+            end = time.time() + timeout
+            cond.wait(timeout)
+            now = time.time()
+            while not pred():
+                if now < end:
+                    cond.wait(end - now)
+                    now = time.time()
+                else:
+                    return False
+            return True
+
+class Reader(object):
+    """Wrap a poller and read completed messages."""
+    def __init__(self, poller, wrappercls):
         """Initialize a ListenReader.
 
         listener: The raw listening socket.
         poller: A `jhsiao.ipc.polling` Poller
         wrappercls: A class to wrap incoming socket.
         """
-        self.listener = listener
         self.poller = poller
-        self.listener.settimeout()
+        self.cond = threading.Condition()
+        self.q = []
 
-    def readinto1(self, out):
-        
+    def _predicate(self):
+        return self.q
+
+    def get(self, timeout=None):
+        """Get any pending messages."""
+        with self.cond:
+            if (not self.q and
+                    not _wait_for_cond(
+                        self.cond, self._predicate, timeout)):
+                return []
+            ret = self.q
+            self.q = []
+            return ret
+
+    def _run(self):
+        """Continually poll for new data.
+
+        When messages are completed, add to self.q
+        """
+        # TODO
