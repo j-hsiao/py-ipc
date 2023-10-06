0ba797a88219101b1a25d80001e006983ec11a5f
Jason Hsiao <jason.hsiao@acer.com>
2023-10-06 17:54:17 +0800

    separating interface/members

--- jhsiao/ipc/threaded/polling/polling.py
+++ jhsiao/ipc/threaded/polling/polling.py
@@ -37,7 +37,7 @@ import threading
 
 from . import rwpair
 
-if sys.version_info > (3,4):
+if sys.version_info > (3,4) and isinstance(threading.Condition, type):
     _wait_cond = threading.Condition.wait_for
 else:
     from jhsiao.ipc import errnos
@@ -93,74 +93,76 @@ class Poller(object):
     """
     def __init__(self):
         self._cond = threading.Condition()
-        self._running = False
+        self._running = True
         self._thread = None
         self._rwpair = rwpair.RWPair()
         self.fileno = self._rwpair.fileno
         self[self] = self.s
+        self.bad = []
 
     def fileno(self):
         return self.fileno()
 
     def readinto1(self, out):
+        """Handle any interrupts of the polling."""
         raise NotImplementedError
 
     def __iter__(self):
         """Iterate on registered items."""
         raise NotImplementedError
 
-    # mainthread interface
     def step(self):
-        """Poll once and incremental read/write."""
+        """Poll once and incremental read/write.
+
+        Return True if good to continue
+        """
         raise NotImplementedError
 
     def start(self):
         """Start a polling thread."""
         with self._cond:
-            if self._running:
+            if self._thread is not None:
                 return
             self._thread = threading.Thread(target=self._run)
             self._running = True
             self._thread.start()
 
     def stop(self):
+        """Stop a polling thread."""
         with self._cond:
             if not self._running:
                 return
             self._running = False
-            self._ctrl.wf.write(b'1')
+            self._rwpair.write(b'1')
             thread = self._thread
         thread.join()
 
     def _run(self):
-        while self._running
-            self.step()
-
-    # Pollthread interface
-    def __delitem__(self, item):
-        """Call unregister."""
-        self.unregister(item)
-
-    def __setitem__(self, item, mode):
-        """Call register."""
-        self.register(item, mode)
+        try:
+            while self.step():
+                pass
+        finally:
+            with self._cond:
+                self._running = False
+                self._thread = None
 
     def unregister(self, item):
-        """Unregister an item."""
-        del self[item]
+        """Unregister an item. public use."""
+        raise NotImplementedError
 
     def register(self, item, mode):
         """Register item under mode.
 
         Re-register to change mode.
+        public use
         """
-        self[item] = mode
+        raise NotImplementedError
 
-    def poll(self, timeout):
-        """Poll objects and return backend-specific object.
-
-        No arg means no timeout.
-        """
+    def __delitem__(self, item):
+        """Unregister in loop."""
+        raise NotImplementedError
+    def __setitem__(self, item, mode):
+        """Register in loop."""
         raise NotImplementedError
 
     def close(self):
@@ -169,172 +171,43 @@ class Poller(object):
         Any registered items are left alone.
         """
         self.stop()
-        del self[self]
         self._rwpair.close()
 
-class _RPoller(object):
+class RPoller(object):
     """Poller with reading functionality."""
     def __init__(self):
-        super(_RPoller, self).__init__()
-        self.rq = []
+        super(RPoller, self).__init__()
+        self.received = []
 
+    def _pred(self):
+        """predicate for condition variable.
 
-    if sys.version_info > (3,4):
-        def _pred(self):
-            """predicate for condition variable.
-
-            Note that using self.rq.__len__ is not enough because
-            another thread may have swapped out rq.  This is necessary
-            to be threadsafe and not double up in case get is called
-            from multiple threads.
-            """
-            return self.rq
-
-        def get(self, timeout=None):
-            """Get list of available data."""
-            with self._cond:
-                if self.rq or self._cond.wait_for(self._pred, timeout):
-                    ret = self.rq
-                    self.rq = []
-                    return ret
-                return ()
-    else:
-        def get(self, timeout=None):
-            """Get list of available data."""
-            with self._cond:
-                if self.rq or _wait_cond(self._cond, self._pred, timeout):
-                    ret = self.rq
-                    self.rq = []
-                    return ret
-                return ()
-
-
-
-
-class RPoller(Poller):
-    """Poll for reads.
-
-    When registering items, read flag is always added and write flag is
-    always ignored.
-    """
-    def __init__(self):
-        self.rq = []
+        Note that using self.received.__len__ is not enough because
+        another thread may have swapped out received.  This is necessary
+        to be threadsafe and not double up in case get is called
+        from multiple threads.
+        """
+        return self.received
 
     def get(self, timeout=None):
-        """Return a list of received messages."""
-        raise NotImplementedError
-
-    def fill(self, pollout, r, out, bad):
-        """Read a little data from each item.
-
-        pollout: poll output
-        r: list of readers
-        out: output container
-        bad: list of bad items.
-        """
-        raise NotImplementedError
+        """Get list of available data."""
+        with self._cond:
+            if self.received or _wait_cond(self._cond, self._pred, timeout):
+                ret = self.received
+                self.received = []
+                return ret
+            return ()
 
 class WPoller(Poller):
     """Write polling object."""
     def __init__(self):
-        self.wq = []
+        super(WPoller, self).__init__()
 
     def write(self, fobj, data):
         """Enqueue a write on fobj with data."""
         raise NotImplementedError
 
-    def fill(self, pollout, w, bad):
-        """Call flush1() on writers.
-
-        w will be updated with writers that have not blocked and have
-        more data towrite.
-        Bad items are added to bad (disconnected or error.)
-        """
-        raise NotImplementedError
-
-class RWPoller(Poller):
+class RWPoller(WPoller, RPoller):
     """Combine read and write polling."""
     def __init__(self):
-        self.rq = []
-        self.wq = []
-
-    def write(self, fobj, data):
-        """Enqueue a write on fobj with data."""
-        raise NotImplementedError
-
-    def get(self, timeout=None):
-        """Return a list of received messages."""
-        raise NotImplementedError
-
-    def fill(self, pollout, r, w, out, bad):
-        """Like WPoller.fill() and RPoller.fill()."""
-        raise NotImplementedError
-
-class RegisterCtrl(RWPair):
-    """Poller control class.
-
-    Generally, objects will either be read from or written to, possibly
-    both.  For reading, objects should probably be registered to be
-    polled and left.  The corresponding readinto1 method should place
-    any complete messages into a queue or list.
-
-    For writers, write polling is only needed if writing would block.
-    As a result, they shouldn't necessarily be directly registered.
-    """
-    def __init__(self, poller, rlist, wlist, sock=None, lock=NullLock()):
-        super(PollRegister, self).__init__(sock)
-        self.lock = lock
-        self.poller = poller
-        for attr in ('r', 'w', 'rw', 's'):
-            setattr(self, attr, getattr(self.poller, attr))
-        self.rlist = rlist
-        self.wlist = wlist
-        self.rq = []
-        self.wq = []
-
-    def write(self, *args):
-        """Register writing some data.
-
-        args: pair of Writer and data.
-             Writer is a `jhsiao.ipc.formats.bases.Writer`.
-             data is some data that would be given to the writer's
-             write() method.
-        """
-        with self.lock:
-            self.wq.append(args)
-            self.wf.write(b'1')
-
-    def register(self, *args):
-        """Add an item to be registered to the poll.
-
-        This should match the argument of poller.register.
-        """
-        with self.lock:
-            self.rq.append(args)
-            self.wf.write(b'1')
-
-    def readinto1(self, out):
-        """Register items."""
-        with self.lock:
-            rq = self.rq
-            self.rq = []
-            wq = self.wq
-            self.wq = []
-        poller = self.poller
-        for args in rq:
-            try:
-                poller.register(*args)
-            except Exception:
-                traceback.print_exc()
-            self.rf.read(1)
-        for w, data in wq:
-            w.write(data)
-            result = w.flush1()
-            if result == None:
-                poller.modify()? register?
-            elif result == -1:
-                pass
-            elif w:
-                pass
-        return -2
-
+        super(RWPoller, self).__init__()
--- jhsiao/ipc/threaded/polling/select.py
+++ jhsiao/ipc/threaded/polling/select.py
@@ -15,39 +15,68 @@ class SelectPoller(object):
     rw = 3
     s = r
 
+    def __init__(self):
+        super(SelectPoller, self).__init__()
+
 class RSelectPoller(SelectPoller, polling.RPoller):
     def __init__(self):
         super(RSelectPoller, self).__init__()
         self.items = set()
-        self.unregister = self.items.discard
+        self._changes = (set(), set())
+        self._ready = []
+
+    def readinto1(self, out):
+        with self._cond:
+            add, rm = self._changes
+            items = self.items
+            items.update(add)
+            items.difference_update(rm)
+            add.clear()
+            rm.clear()
 
     def __iter__(self):
         return iter(self.items)
 
+    def __delitem__(self, item):
+        self.items.discard(item)
     def __setitem__(self, item, mode):
         self.items.add(item)
 
+    def register(self, item, mode):
+        with self._cond:
+            toadd, torm = self._changes
+            toadd.add(item)
+            torm.discard(item)
+
     def unregister(self, item):
-        self.items.discard(item)
+        with self._cond:
+            toadd, torm = self._changes
+            toadd.discard(item)
+            torm.add(item)
 
-    def poll(self, timeout=None):
-        return select.select(self.items, (), (), timeout)[0]
+    def step(self):
+        ready = self._ready
+        items = self.items
+        r, w, x = select.select(items, (), (), 0 if self._ready else None)
+        if r:
+            items.difference_update(r)
+            ready.extend(r)
+        with self._cond:
+            i = 0
+            received = self.received
+            for item in ready:
+                result = item.readinto1(received)
+                if result is None or result == -2:
+                    items.add(item)
+                elif result == -1:
+                    self.bad.append(item)
+                else:
+                    ready[i] = item
+                    i += 1
+            ret = self._running:
+        del ready[i:]
+        return ret
 
-    def fill(self, pollout, r, out, bad):
-        if pollout:
-            self.items.difference_update(pollout)
-            r.extend(pollout)
-        i = 0
-        for item in r:
-            result = item.readinto1(out)
-            if result is None or result == -2:
-                self.items.add(item)
-            elif result == -1:
-                bad.append(item)
-            else:
-                r[i] = item
-                i += 1
-        del r[i:]
 
 class _RW(SelectPoller):
     """Base class with _r and _w set attrs."""
